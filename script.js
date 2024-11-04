01ac99f1db2ddf8d375564f5f1df7620838be791
Hugo <122971194+hugogilt@users.noreply.github.com>
2024-11-04 11:07:02 +0100

    Añado un capital inicial máximo para cada banco
    Me baso en la realidad, el máximo de trade republic son 50.000€, mientras que de revolut son 100.000€

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ function calcular() {
     const interesRevolutDiario = interesRevolutAnual / 365;
 
     // Cálculo para Trade Republic (interés compuesto mensual) TOFIX: TENER EN CUENTA QUE EN TRADE EL MAXIMO CAPITAL ES 50.000 Y EN REVOLUT 100.000
-    let capitalTradeRepublicFinal = capitalInicial * Math.pow((1 + interesTradeRepublicMensual), 12);
-    let beneficioTradeRepublicSinImpuestos = capitalTradeRepublicFinal - capitalInicial;
+    let capitalInicialTradeRepublic;
+    let alertaTradeRepublic = '';
+    if (capitalInicial > 50000) {
+        capitalInicialTradeRepublic = 50000;
+        alertaTradeRepublic = 'Los intereses han sido aplicados sobre 50.000€'
+    } else {
+        capitalInicialTradeRepublic = capitalInicial;
+    }
+
+    let capitalTradeRepublicFinal = capitalInicialTradeRepublic * Math.pow((1 + interesTradeRepublicMensual), 12);
+    let beneficioTradeRepublicSinImpuestos = capitalTradeRepublicFinal - capitalInicialTradeRepublic;
     let beneficioTradeRepublicConImpuestos = beneficioTradeRepublicSinImpuestos * (1 - impuesto);
 
     // Cálculo para Revolut (interés compuesto diario)
-    let capitalRevolutFinal = capitalInicial;
+    let capitalInicialRevolut;
+    let alertaRevolut = '';
+    if (capitalInicial > 100000) {
+        capitalInicialRevolut = 100000;
+         alertaRevolut = 'Los intereses han sido aplicados sobre 100.000€'
+    } else {
+        capitalInicialRevolut = capitalInicial;
+    }
+
+    let capitalRevolutFinal = capitalInicialRevolut;
     for (let i = 0; i < 365; i++) {
         let interesDiario = capitalRevolutFinal * interesRevolutDiario;
         if (impuestos) {
@@ -24,7 +42,7 @@ function calcular() {
         }
         capitalRevolutFinal += interesDiario;
     }
-    let beneficioRevolutConImpuestos = capitalRevolutFinal - capitalInicial;
+    let beneficioRevolutConImpuestos = capitalRevolutFinal - capitalInicialRevolut;
 
     // Diferencia de beneficios
     let diferenciaTradeRevolut;
@@ -32,7 +50,7 @@ function calcular() {
     if (impuestos) {
         diferenciaTradeRevolut = beneficioTradeRepublicConImpuestos - beneficioRevolutConImpuestos;
     } else {
-        diferenciaTradeRevolut = beneficioTradeRepublicSinImpuestos - (capitalRevolutFinal - capitalInicial);
+        diferenciaTradeRevolut = beneficioTradeRepublicSinImpuestos - (capitalRevolutFinal - capitalInicialRevolut);
     }
 
     // Mostrar el resultado según si es positivo o negativo
@@ -48,19 +66,33 @@ function calcular() {
         document.getElementById("resultado").innerHTML = `
             <p><strong>Con Impuestos:</strong></p>
             <p>Capital final Trade Republic: ${capitalTradeRepublicFinal.toFixed(2)}€</p>
-            <p>Beneficio neto anual con Trade Republic: ${beneficioTradeRepublicConImpuestos.toFixed(2)}€</p>
+            <p>Beneficio neto anual con Trade Republic: ${beneficioTradeRepublicConImpuestos.toFixed(2)}€
+            <span class = alerta>${alertaTradeRepublic}<img id="alertaTradeRepublicImg" src="./img/warning.png"></span></p>
             <p>Capital final Revolut: ${capitalRevolutFinal.toFixed(2)}€</p>
-            <p>Beneficio neto anual con Revolut: ${beneficioRevolutConImpuestos.toFixed(2)}€</p>
+            <p>Beneficio neto anual con Revolut: ${beneficioRevolutConImpuestos.toFixed(2)}€
+            <span class = alerta>${alertaRevolut}<img id="alertaRevolutImg" src="./img/warning.png"></span></p>
             <p>${mensajeDiferencia}</p>
         `;
     } else {
         document.getElementById("resultado").innerHTML = `
             <p><strong>Sin Impuestos:</strong></p>
             <p>Capital final Trade Republic: ${capitalTradeRepublicFinal.toFixed(2)}€</p>
-            <p>Beneficio neto Trade Republic: ${beneficioTradeRepublicSinImpuestos.toFixed(2)}€</p>
+            <p>Beneficio neto Trade Republic: ${beneficioTradeRepublicSinImpuestos.toFixed(2)}€
+            <span class = alerta>${alertaTradeRepublic}<img id="alertaTradeRepublicImg" src="./img/warning.png"></span></p>
             <p>Capital final Revolut: ${capitalRevolutFinal.toFixed(2)}€</p>
-            <p>Beneficio neto Revolut: ${(capitalRevolutFinal - capitalInicial).toFixed(2)}€</p>
+            <p>Beneficio neto Revolut: ${(capitalRevolutFinal - capitalInicialRevolut).toFixed(2)}€
+            <span class = alerta>${alertaRevolut}<img id="alertaRevolutImg" src="./img/warning.png"></span></p>
             <p>${mensajeDiferencia}</p>
         `;
     }
+    function ocultarImagenSiAlertaVacia(alerta, idImagen) {
+        if (alerta === '') {
+            const imagen = document.querySelector(`#${idImagen}`);
+            if (imagen) {
+                imagen.style.display = 'none';
+            }
+        }
+    }
+    ocultarImagenSiAlertaVacia(alertaTradeRepublic, 'alertaTradeRepublicImg');
+    ocultarImagenSiAlertaVacia(alertaRevolut, 'alertaRevolutImg');
 }
